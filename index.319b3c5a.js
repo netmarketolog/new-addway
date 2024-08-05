176b9aa5c74698427ce4f57c1bb9fe7bd97fd2d2
netmarketolog <99245672+netmarketolog@users.noreply.github.com>
2024-08-05 20:04:39 +0000

    Deploying to gh-pages from @ netmarketolog/new-addway@38c19de9a20e761fe7480306f3cb51b6144ba8ac 🚀

---
+++ index.319b3c5a.js
@@ -0,0 +1,2 @@
+$(document).ready(function(){$(".slider__wrapper").slick({slidesToShow:1,slidesToScroll:1,arrows:!0,dots:!0})});
+//# sourceMappingURL=index.319b3c5a.js.map
---
+++ index.da0707f6.js
@@ -0,0 +1,2 @@
+$(document).ready(function(){$(".slider__wrapper").slick({slidesToShow:1,slidesToScroll:1,arrows:!0,dots:!0})});
+//# sourceMappingURL=index.da0707f6.js.map
