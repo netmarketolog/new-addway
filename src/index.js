895a0359358b8f9afc677ff36102a76e55050584
Vetali1985 <100028292+Vetali1985@users.noreply.github.com>
2024-08-09 10:42:52 +0300

    fix style

--- src/index.js
+++ src/index.js
@@ -4,35 +4,3 @@ import './js/header';
 import getBreadcrumbs from './js/breadcrumbs';
 
 getBreadcrumbs();
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, get } from "firebase/database";
-
-   // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBSPg3ScFTycIB2Unf4f7_QQ1IuNa-HR0c",
-  authDomain: "addway-ecaed.firebaseapp.com",
-  projectId: "addway-ecaed",
-  storageBucket: "addway-ecaed.appspot.com",
-  messagingSenderId: "717099611397",
-  appId: "1:717099611397:web:3b59ab65c185e28625b36c",
-  measurementId: "G-70MT7EQJME",
- databaseURL: "https://addway-ecaed-default-rtdb.europe-west1.firebasedatabase.app", 
-};
-
-
-   // Initialize Firebase
-   const app = initializeApp(firebaseConfig);
-   const database = getDatabase(app);
-
-   // Get data from Firebase
-   get(ref(database, 'botConfig')).then((snapshot) => {
-     if (snapshot.exists()) {
-       const data = snapshot.val();
-       document.body.setAttribute('data-bot-token', data.botToken);
-       document.body.setAttribute('data-chat-id', data.chatId);
-     } else {
-       console.log("No data available");
-     }
-   }).catch((error) => {
-     console.error(error);
-   });
--- src/js/app.js
+++ src/js/app.js
@@ -16,16 +16,16 @@ const firebaseConfig = {
 // Initialize Firebase
 let app;
 if (!getApps().length) {
-  console.log("Initializing Firebase with config:", firebaseConfig);
+  // console.log("Initializing Firebase with config:", firebaseConfig);
   app = initializeApp(firebaseConfig);
-  console.log("Firebase app initialized:", app);
+  // console.log("Firebase app initialized:", app);
 } else {
   app = getApp();
-  console.log("Using existing Firebase app:", app);
+  // console.log("Using existing Firebase app:", app);
 }
 
 const database = getDatabase(app);
-console.log("Database initialized:", database);
+// console.log("Database initialized:", database);
 
 // Глобальные переменные для хранения данных
 window.botToken = null;
@@ -33,18 +33,18 @@ window.chatId = null;
 
 // Get data from Firebase
 const botConfigRef = databaseRef(database, 'botConfig');
-console.log("Reference to botConfig:", botConfigRef);
+// console.log("Reference to botConfig:", botConfigRef);
 
 get(botConfigRef).then((snapshot) => {
-  console.log("Snapshot:", snapshot);
+  // console.log("Snapshot:", snapshot);
   const data = snapshot.val();
-  console.log("Data from snapshot.val():", data);
+  // console.log("Data from snapshot.val():", data);
   if (data) {
-    console.log("Data exists");
+    // console.log("Data exists");
     window.botToken = typeof data.botToken === 'string' ? data.botToken.replace(/"/g, '') : data.botToken;
     window.chatId = typeof data.chatId === 'string' ? data.chatId.replace(/"/g, '') : data.chatId;
-    console.log("botToken:", window.botToken);
-    console.log("chatId:", window.chatId);
+    // console.log("botToken:", window.botToken);
+    // console.log("chatId:", window.chatId);
 
     // Генерируем событие после загрузки данных
     const event = new CustomEvent('firebaseDataLoaded', {
@@ -55,8 +55,8 @@ get(botConfigRef).then((snapshot) => {
     });
     window.dispatchEvent(event);
   } else {
-    console.log("No data available");
+    // console.log("No data available");
   }
 }).catch((error) => {
-  console.error("Error retrieving data from Firebase:", error);
+  // console.error("Error retrieving data from Firebase:", error);
 });
